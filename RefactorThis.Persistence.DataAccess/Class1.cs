be86d313e8ef3bf691d079713c91938e78da8894
Denuka Kumarage <denuka.kumarage>
2021-11-10 22:43:45 +0530

    Adding Seperate Layers to address seperate of concerns

---
+++ RefactorThis.Persistence.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RefactorThis.Persistence.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ RefactorThis.Persistence.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RefactorThis.Persistence.Repository
+{
+    public class Class1
+    {
+    }
+}
